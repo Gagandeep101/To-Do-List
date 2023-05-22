1a5ad71e309844186505a1c1b2a335661f876b7c
Gagandeep_Singh_101 <ts026925@gmail.com>
2023-05-22 13:26:36 +0530

    feat : add click event listener on add btn
    In js if input field in empty then show alert message otherwise we create an element of li and span and then span append in li further li append in tasks
    container . Clear input field and save data in localstorage

--- brain.js
+++ brain.js
@@ -1,3 +1,19 @@
 var input_task = document.querySelector('#input_task');
 var add_task = document.querySelector('#add_task');
 var task_area = document.querySelector('.task_shown_area');
+add_task.addEventListener('click', addtask);
+function addtask() {
+    if (input_task.value === "") {
+        alert("Write a task to add in lists");
+    }
+    else {
+        var li = document.createElement('li');
+        var span = document.createElement('span');
+        span.innerHTML="&cross;";
+        li.innerHTML = input_task.value;
+        li .appendChild(span);
+        task_area.appendChild(li);
+    }
+    input_task.value="";
+    savedata();
+}
